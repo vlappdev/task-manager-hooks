8ce09c5b315ac2da7afb929cc250fb73e0a95f8b
jahorinac <registracijae@gmail.com>
2020-06-16 10:56:27 +0200

    Updates

---
+++ precache-manifest.de9f7760dac5f7fdaa02deafc50aa795.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7ec5704fe644010037d95f58370c83",
+    "url": "/03-hooks-react/index.html"
+  },
+  {
+    "revision": "80fbce6cad86b18ced36",
+    "url": "/03-hooks-react/static/css/2.4c62a1fd.chunk.css"
+  },
+  {
+    "revision": "fa1add07321119075f53",
+    "url": "/03-hooks-react/static/css/main.09965f23.chunk.css"
+  },
+  {
+    "revision": "80fbce6cad86b18ced36",
+    "url": "/03-hooks-react/static/js/2.0b1441a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/03-hooks-react/static/js/2.0b1441a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1add07321119075f53",
+    "url": "/03-hooks-react/static/js/main.0e1875e6.chunk.js"
+  },
+  {
+    "revision": "44ec5642798aa9bd7e27",
+    "url": "/03-hooks-react/static/js/runtime-main.f7cfcdca.js"
+  },
+  {
+    "revision": "b7d6958d140ca2c5847ea9edc09d73bd",
+    "url": "/03-hooks-react/static/media/icon-add-task.b7d6958d.svg"
+  },
+  {
+    "revision": "076673e1fff955a16b1385be8e0485ba",
+    "url": "/03-hooks-react/static/media/icon-add-user.076673e1.svg"
+  },
+  {
+    "revision": "a86adb135eeb16799143615a1c6f0f53",
+    "url": "/03-hooks-react/static/media/icon-alert.a86adb13.svg"
+  },
+  {
+    "revision": "8ff6e68d5b401406f38fcb6882cb230e",
+    "url": "/03-hooks-react/static/media/icon-attach.8ff6e68d.svg"
+  },
+  {
+    "revision": "9bf5f99a9c5924723d89f2c92529e553",
+    "url": "/03-hooks-react/static/media/icon-avatar.9bf5f99a.svg"
+  },
+  {
+    "revision": "d51ded8bb6b01fe5b1f6e31767972895",
+    "url": "/03-hooks-react/static/media/icon-boards.d51ded8b.svg"
+  },
+  {
+    "revision": "55f6eff1e573ed18fc8f07056c627a62",
+    "url": "/03-hooks-react/static/media/icon-mail.55f6eff1.svg"
+  },
+  {
+    "revision": "1679b905320e35942f4875075e297ce1",
+    "url": "/03-hooks-react/static/media/icon-manage.1679b905.svg"
+  },
+  {
+    "revision": "b80042c6ee4e39b974583f4eecc18184",
+    "url": "/03-hooks-react/static/media/icon-message.b80042c6.svg"
+  },
+  {
+    "revision": "33479d249bdf6b6ab40984d66063e1ef",
+    "url": "/03-hooks-react/static/media/icon-reports.33479d24.svg"
+  },
+  {
+    "revision": "c643414abb5d8bdfb9c21e09ea7436c5",
+    "url": "/03-hooks-react/static/media/icon-schedule.c643414a.svg"
+  },
+  {
+    "revision": "dabce41e9a6a39290e14c4043dff1bc1",
+    "url": "/03-hooks-react/static/media/icon-search.dabce41e.svg"
+  },
+  {
+    "revision": "a91576d75c00b454c841db85d9f5ba5e",
+    "url": "/03-hooks-react/static/media/icon-three-dots.a91576d7.svg"
+  }
+]);
