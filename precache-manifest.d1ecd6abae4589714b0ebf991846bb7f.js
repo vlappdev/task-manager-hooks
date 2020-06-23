5aee8f787a718e0c382c8135499e3802bd99b193
vlappdev <vladimir.eoffice@gmail.com>
2020-06-23 12:41:48 +0200

    Updates

---
+++ precache-manifest.d1ecd6abae4589714b0ebf991846bb7f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c420157cd5a617f221f61f456bea51",
+    "url": "/task-manager-hooks/index.html"
+  },
+  {
+    "revision": "78e5ce5006a95cf3f62c",
+    "url": "/task-manager-hooks/static/css/2.4c62a1fd.chunk.css"
+  },
+  {
+    "revision": "12f817179ef4c2f674ad",
+    "url": "/task-manager-hooks/static/css/main.a724737d.chunk.css"
+  },
+  {
+    "revision": "78e5ce5006a95cf3f62c",
+    "url": "/task-manager-hooks/static/js/2.dd2a102a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-manager-hooks/static/js/2.dd2a102a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f817179ef4c2f674ad",
+    "url": "/task-manager-hooks/static/js/main.c2054979.chunk.js"
+  },
+  {
+    "revision": "abce541549c033dec661",
+    "url": "/task-manager-hooks/static/js/runtime-main.2d86ddcb.js"
+  },
+  {
+    "revision": "b7d6958d140ca2c5847ea9edc09d73bd",
+    "url": "/task-manager-hooks/static/media/icon-add-task.b7d6958d.svg"
+  },
+  {
+    "revision": "076673e1fff955a16b1385be8e0485ba",
+    "url": "/task-manager-hooks/static/media/icon-add-user.076673e1.svg"
+  },
+  {
+    "revision": "a86adb135eeb16799143615a1c6f0f53",
+    "url": "/task-manager-hooks/static/media/icon-alert.a86adb13.svg"
+  },
+  {
+    "revision": "8ff6e68d5b401406f38fcb6882cb230e",
+    "url": "/task-manager-hooks/static/media/icon-attach.8ff6e68d.svg"
+  },
+  {
+    "revision": "9bf5f99a9c5924723d89f2c92529e553",
+    "url": "/task-manager-hooks/static/media/icon-avatar.9bf5f99a.svg"
+  },
+  {
+    "revision": "d51ded8bb6b01fe5b1f6e31767972895",
+    "url": "/task-manager-hooks/static/media/icon-boards.d51ded8b.svg"
+  },
+  {
+    "revision": "55f6eff1e573ed18fc8f07056c627a62",
+    "url": "/task-manager-hooks/static/media/icon-mail.55f6eff1.svg"
+  },
+  {
+    "revision": "1679b905320e35942f4875075e297ce1",
+    "url": "/task-manager-hooks/static/media/icon-manage.1679b905.svg"
+  },
+  {
+    "revision": "b80042c6ee4e39b974583f4eecc18184",
+    "url": "/task-manager-hooks/static/media/icon-message.b80042c6.svg"
+  },
+  {
+    "revision": "c643414abb5d8bdfb9c21e09ea7436c5",
+    "url": "/task-manager-hooks/static/media/icon-schedule.c643414a.svg"
+  },
+  {
+    "revision": "dabce41e9a6a39290e14c4043dff1bc1",
+    "url": "/task-manager-hooks/static/media/icon-search.dabce41e.svg"
+  },
+  {
+    "revision": "a91576d75c00b454c841db85d9f5ba5e",
+    "url": "/task-manager-hooks/static/media/icon-three-dots.a91576d7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/03-hooks-react/precache-manifest.cd0da96b09752d8a32b97cd0e7e3ba90.js"
+  "/task-manager-hooks/precache-manifest.d1ecd6abae4589714b0ebf991846bb7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/03-hooks-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/task-manager-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
